2d000b55120353de5941cd6fea5a5997f27c2eb6
rdoru234 <76903690+rdoru234@users.noreply.github.com>
2023-08-12 12:14:38 -0400

    1

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -43,4 +43,24 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
 });
 
 
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
+     }, 1200, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
 
